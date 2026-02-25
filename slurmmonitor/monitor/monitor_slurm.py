d2a603ad417d75b6c0eb9951c5da1cf303cf22e9
tylerphilp <tyler116@mit.edu>
2026-02-25 14:11:31 -0500

    :bug: Adding kill logic

--- slurmmonitor/monitor/monitor_slurm.py
+++ slurmmonitor/monitor/monitor_slurm.py
@@ -6,6 +6,8 @@
 # %%
 import subprocess
 import time
+import signal
+import sys
 from slurmmonitor.slackbot.messenger import SlackMessenger
 
 # %%
@@ -15,8 +17,12 @@ class SlurmMonitor:
     def __init__(self, job_id):
         self.job_id = job_id
         self.messenger = SlackMessenger()
-        self.last_status = "PENDING"  # self.get_job_status()
+        self.last_status = self.get_job_status()
 
+        # Hande timeouts and kill signal
+        signal.signal(signal.SIGTERM, self.handle_termination)
+
+        # Intialisation method
         self.messenger.send_message(
             f"✨ SlurmMonitor initialised for job: {self.job_id}. Current status: {self.last_status}"
         )
@@ -34,10 +40,18 @@ class SlurmMonitor:
         while True:
             status = self.get_job_status()
             if status != self.last_status:
-                self.messenger.send_message(f"Job {self.job_id} status: {status}")
+                self.messenger.send_message(f"⏳ Job {self.job_id} status: {status}")
                 self.last_status = status
                 if status == "COMPLETED":
                     self.messenger.send_message(f"🎉 Job {self.job_id} {status}")
                     break
 
             time.sleep(poll_interval)
+
+        # Need to add some logic for when the job is killed (since this is initialised in the job)
+
+    def handle_termination(self, signum, frame):
+        self.messenger.send_message(
+            f"❌ Job {self.job_id} was terminated (signal {signum}). Likely killed or timed out."
+        )
+        sys.exit(0)
